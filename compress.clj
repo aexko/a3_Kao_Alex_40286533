9b8ddd31d0699d5144b7cb546e50adefab92b5e1
Alex Kao <ab.kao@hotmail.com>
2023-12-05 22:36:22 -0500

    omg decompress works

--- compress.clj
+++ compress.clj
@@ -50,23 +50,39 @@
         (recur (str new-string (nth (slice file-name) current-index) " ") (inc current-index)))
       new-string)))
 
-;; (println (compress "t1.txt"))
+; (println (compress "t1.txt"))
+
+; Function that gets the value of the key in the map
+(defn get-value
+  [map key]
+  (get map key))
+(println (get-value main-map-words 0))
+
+; Function that gets the key of the value in the map as int
+(defn get-key
+  [map value]
+  (first (filter #(= value (val %)) map)))
+(println (get (get-key main-map-words "the") 0))
+
+; https://stackoverflow.com/questions/5621279/in-clojure-how-can-i-convert-a-string-to-a-number
+(defn parse-int [s]
+  (Integer. (re-find  #"\d+" s)))
 
 
 ; Function that decompresses a file with the help of a map called
-; "main-map-words" that contains the words and their index. It slices the file
-; into a list of numbers and replaces the numbers of the list with the words from the map.
-; This list is then converted into a string of words
-; It returns a string of words
+; "main-map-words". It uses regex to find the numbers in the file and
+; replaces them with the word that has that index in the map.
 (defn decompress [file_name]
-  (loop [list-number [] current-index 0]
+  (loop [new-string "" current-index 0]
     ; checks if the current-index is less than the length of the list
     (if (< current-index (count (slice file_name)))
-      ; checks the number of the word in the list
-      (if (number? (read-string (nth (slice file_name) current-index)))
-        ; if it's a number, it adds it to the list
-        (recur (conj list-number (nth (slice file_name) current-index)) (inc current-index))
-        ; if it's not a number, it doesn't add it to the list
-        (recur list-number (inc current-index)))
-      list-number)))
-  (println (decompress "t1_number.txt"))
+      ; checks if the word is a number
+      (if (re-matches #"\d+" (nth (slice file_name) current-index))
+        ; if it's a number, it replaces the number with the word that has that index in the map
+        (recur (str new-string (get main-map-words (parse-int (nth (slice file_name) current-index))) " ") (inc current-index))
+        ; if it's not a number, it doesn't replace it with anything
+        (recur (str new-string (nth (slice file_name) current-index) " ") (inc current-index)))
+      new-string)))
+ 
+
+(println (decompress "t1_number.txt"))
